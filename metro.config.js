dd131e48a049de5d85f5b4353310622987ea0894
Moazam-Saif <msaif.bese23seecs@seecs.edu.pk>
2025-12-05 04:58:52 +0500

    path aliasing issue

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,13 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Add support for @ alias
+config.resolver.extraNodeModules = {
+  '@': __dirname,
+};
 
 module.exports = config;
