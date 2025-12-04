992c7f6450499619f7be230acf8a88b6f861515c
Moazam-Saif <msaif.bese23seecs@seecs.edu.pk>
2025-12-05 04:53:56 +0500

    ...

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './',
+          },
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx',
+            '.web.js',
+            '.web.tsx',
+          ],
+        },
+      ],
+    ],
+  };
+};
--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-// Configure path aliases for Metro bundler
-config.resolver = {
-  ...config.resolver,
-  extraNodeModules: new Proxy(
-    {},
-    {
-      get: (target, name) => {
-        if (name === '@') {
-          return path.resolve(__dirname);
-        }
-        return path.join(__dirname, 'node_modules', name);
-      },
-    }
-  ),
-};
 
 module.exports = config;
